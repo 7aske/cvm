befb874899ed25d07b51b1ae650f53a4dbda123b
Nikola Tasic <ntasic7@gmail.com>
2019-11-15 11:07:09 +0100

    working jump back instr

---
+++ opcode.h
@@ -0,0 +1,226 @@
+//
+// Created by nik on 11/13/19.
+//
+
+#ifndef CVM_OPCODE_H
+#define CVM_OPCODE_H
+
+#pragma once
+
+#include <stdint.h>
+
+
+typedef enum opcodes {
+	// AAA = 0x37,                  // /*ASCII adjust AL after addition*/
+	// AAD = 0xD5,                  // /*ASCII adjust AX before division*/
+	// AAM = 0xD4,                  // /*ASCII adjust AX after multiplication*/
+	// AAS = 0x3F,                  // /*ASCII adjust AL after subtraction*/
+	// ADC = 0x10,                  // 0x15 0x80 0x83 /*Add with carry*/
+			ADD = 0x00,                    // 0x05 0x80 0x83 /*Add*/
+	AND = 0x20,                    // 0x25 0x80 0x83 /*Logical AND*/
+	CALL = 0x9A,                    // 0xE8 0xFF 0xFF /*Call procedure*/
+	// CBW = 0x98,                  // /*Convert byte to word*/
+	// CLC = 0xF8,                  // /*Clear carry flag*/
+	// CLD = 0xFC,                  // /*Clear direction flag*/
+	// CLI = 0xFA,                  // /*Clear interrupt flag*/
+	// CMC = 0xF5,                  // /*Complement carry flag*/
+	// CMP = 0x38,                  // 0x3D 0x80 0x83 /*Compare operands*/
+	// CMPSB = 0xA6,                // /*Compare bytes in memory*/
+	// CMPSW = 0xA7,                // /*Compare words*/
+	// CWD = 0x99,                  // /*Convert word to doubleword*/
+	// DAA = 0x27,                  // /*Decimal adjust AL after addition*/
+	// DAS = 0x2F,                  // /*Decimal adjust AL after subtraction*/
+			DEC = 0x48,                    // 0x4F 0xFE 0xFF /*Decrement by 1*/
+	DIV = 0xF6,                    // 0xF7 /*Unsigned divide*/
+	// ESC = 0xD8,                  // 0xDF /*Used with floating-point unit*/
+			HLT = 0xF4,                    // /*Enter halt state*/
+	// IDIV = 0xF7,                  // 0xF7 /*Signed divide*/
+	// IMUL = 0x69,                  	// 0x6B 0xF6 0xF7 0x0F /*Signed multiply*/
+	// IN = 0xE4,                   // 0xE5 0xEC 0xED /*Input from port*/
+			INC = 0x40,                    // 0x47 0xFE 0xFF /*Increment by 1*/
+	// INT = 0xCC,                  // 0xCD /*Call to interrupt*/
+	// INTO = 0xCE,                 // /*Call to interrupt if overflow*/
+	// IRET = 0xCF,                 // /*Return from interrupt*/
+	// JA = 0x77, 						// Jump short if above (CF=0 and ZF=0).
+	// JAE = 0x73, 					// Jump short if above or equal (CF=0).
+	// JB = 0x72, 						// Jump short if below (CF=1).
+	// JBE = 0x76, 					// Jump short if below or equal (CF=1 or ZF=1).
+	// JC = 0x72, 					// Jump short if carry (CF=1).
+	// JCXZ = 0xE3, 				// Jump short if CX register is 0.
+	// JECXZ = 0xE3, 				// Jump short if ECX register is 0.
+			JE = 0x74,                        // Jump short if equal (ZF=1).
+	JG = 0x7F,                        // Jump short if greater (ZF=0 and SF=OF).
+	JGE = 0x7D,                    // Jump short if greater or equal (SF=OF).
+	JL = 0x7C,                        // Jump short if less (SF<>OF).
+	JLE = 0x7E,                    // Jump short if less or equal (ZF=1 or SF<>OF).
+	// JNA = 0x76, 					// Jump short if not above (CF=1 or ZF=1).
+	// JNAE = 0x72, 				// Jump short if not above or equal (CF=1).
+	// JNB = 0x73, 					// Jump short if not below (CF=0).
+	// JNBE = 0x77, 				// Jump short if not below or equal (CF=0 and ZF=0).
+	// JNC = 0x73, 					// Jump short if not carry (CF=0).
+	// JNE = 0x75, 					// Jump short if not equal (ZF=0).
+	// JNG = 0x7E, 					// Jump short if not greater (ZF=1 or SF<>OF).
+	// JNGE = 0x7C, 				// Jump short if not greater or equal (SF<>OF).
+	// JNL = 0x7D, 					// Jump short if not less (SF=OF).
+	// JNLE = 0x7F, 				// Jump short if not less or equal (ZF=0 and SF=OF).
+	// JNO = 0x71, 					// Jump short if not overflow (OF=0).
+	// JNP = 0x7B, 					// Jump short if not parity (PF=0).
+	// JNS = 0x79, 					// Jump short if not sign (SF=0).
+	// JNZ = 0x75, 					// Jump short if not zero (ZF=0).
+	// JO = 0x70, 					// Jump short if overflow (OF=1).
+	// JP = 0x7A, 					// Jump short if parity (PF=1).
+	// JPE = 0x7A, 					// Jump short if parity even (PF=1).
+	// JPO = 0x7B, 					// Jump short if parity odd (PF=0).
+	// JS = 0x78, 					// Jump short if sign (SF=1).
+			JZ = 0x74,                        // Jump short if zero (ZF = 1).
+	// JCXZ = 0xE3,                 // /*Jump if CX is zero*/
+			JMP = 0xE9,                    // 0xEB 0xFF 0xFF /*Jump*/
+	JMPB = 0xE8,                    // 0xEB 0xFF 0xFF /*Jump*/
+	// LAHF = 0x9F,                 // /*Load FLAGS into AH register*/
+	// LDS = 0xC5,                  // /*Load pointer using DS*/
+	// LEA = 0x8D,                  // /*Load Effective Address*/
+	// LES = 0xC4,                  // /*Load ES with pointer*/
+	// LOCK = 0xF0,                 // /*Assert BUS LOCK# signal*/
+	// LODSB = 0xAC,                // /*Load string byte*/
+	// LODSW = 0xAD,                // /*Load string word*/
+	// LOOP = 0xE0,                 // 0xE2 /*Loop control*/
+			MOV = 0xA0,                    // 0xA3 /*Move*/
+	// MOVSB = 0xA4,                // /*Move byte from string to string*/
+	// MOVSW = 0xA5,                // /*Move word from string to string*/
+			MUL = 0x6B,                    // 0xF7 /*Unsigned multiply*/
+	// NEG = 0xF7,                  	// 0xF7 /*Two's complement negation*/
+			NOP = 0x90,                    // /*No operation*/
+	NOT = 0xF7,                    // 0xF7 /*Negate the operand, logical NOT*/
+	OR = 0x08,                    // 0x0D 0x80 0x83 /*Logical OR*/
+	// OUT = 0xE6,                  // 0xE7 0xEE 0xEF /*Output to port*/
+			POP = 0x07,                    // 0x0F 0x17 0x1F 0x58 0x5F 0x8F /*Pop data from stack*/
+	// POPF = 0x9D,                 // /*Pop FLAGS register from stack*/
+			PUSH = 0x06,                    // 0x0E 0x16 0x1E 0x50 0x57 0x68 0x6A 0xFF /*Push data onto stack*/
+	// PUSHF = 0x9C,                // /*Push FLAGS onto stack*/
+	// RCL = 0xC0,                  // 0xC1 0xD0 0xD3 /*Rotate left (with carry)*/
+	// RCR = 0xC0,                  // 0xC1 0xD0 0xD3 /*Rotate right (with carry)*/
+	// REPxx = 0xF2,                // 0xF3 /*Repeat MOVS/STOS/CMPS/LODS/SCAS*/
+			RET = 0xCA,                            /*,//Return from procedure*/
+	// RETN = 0xC2,                    // 0xC3 /*Return from near procedure*/
+	// RETF = 0xCA,                    // 0xCB /*Return from far procedure*/
+	// ROL = 0xC0,                  // 0xC1 0xD0 0xD3 /*Rotate left*/
+	// ROR = 0xC0,                  // 0xC1 0xD0 0xD3 /*Rotate right*/
+	// SAHF = 0x9E,                 // /*Store AH into FLAGS*/
+	// SAL = 0xC0,                  	// 0xC1 0xD0 0xD3 /*Shift Arithmetically left (signed shift left)*/
+	// SAR = 0xC1,                  	// 0xC1 0xD0 0xD3 /*Shift Arithmetically right (signed shift right)*/
+	// SBB = 0x18,                  	// 0x1D 0x80 0x83 /*Subtraction with borrow*/
+	// SCASB = 0xAE,                // /*Compare byte string*/
+	// SCASW = 0xAF,                // /*Compare word string*/
+			SHL = 0xD0,                    // 0xC1 0xD0 0xD3 /*Shift left (unsigned shift left)*/
+	SHR = 0xD1,                    // 0xC1 0xD0 0xD3 /*Shift right (unsigned shift right)*/
+	// STC = 0xF9,                  // /*Set carry flag*/
+	// STD = 0xFD,                  // /*Set direction flag*/
+	// STI = 0xFB,                  // /*Set interrupt flag*/
+	// STOSB = 0xAA,                // /*Store byte in string*/
+	// STOSW = 0xAB,                // /*Store word in string*/
+			SUB = 0x28,                    // 0x2D 0x80 0x83 /*Subtraction*/
+	TEST = 0x84,                    // 0x84 0xA8 0xA9 0xF6 0xF7 /*Logical compare (AND)*/
+	// WAIT = 0x9B,                 // /*Wait until not busy*/
+	// XCHG = 0x86,                 // 0x87 0x91 0x97 /*Exchange data*/
+	// XLAT = 0xD7,                 // /*Table look-up translation*/
+			XOR = 0x30,                    // 0x35 0x80 0x83 /*Exclusive OR*/
+	PUT = 0xFA,
+	PUTC = 0xFB,
+	STOR = 0xFC,
+	LOAD = 0xFD,
+
+} opcode_t;
+
+
+static opcode_t decode(uint8_t* code) {
+	opcode_t opcode = *code;
+	return opcode;
+}
+
+#include <stdio.h>
+
+extern uint32_t next_32(uint8_t* code) {
+	uint8_t temp = *code;
+	int32_t val = (uint32_t) (temp >> 24u);
+	code++;
+	temp = *code;
+	val += (uint32_t) (temp >> 16u);
+	code++;
+	temp = *code;
+	val += (uint32_t) (temp >> 8u);
+	code++;
+	temp = *code;
+	val += (uint32_t) temp;
+	return val;
+}
+
+extern char* mnemonic(opcode_t opcode) {
+	switch (opcode) {
+		case ADD:
+			return "ADD";
+		case AND:
+			return "AND";
+		case CALL:
+			return "CALL";
+		case DEC:
+			return "DEC";
+		case DIV:
+			return "DIV";
+		case HLT:
+			return "HLT";
+		case INC:
+			return "INC";
+		case JMP:
+			return "JMP";
+		case JMPB:
+			return "JMPB";
+		case MOV:
+			return "MOV";
+		case NOP:
+			return "NOP";
+		case OR:
+			return "OR";
+		case POP:
+			return "POP";
+		case PUSH:
+			return "PUSH";
+		case SUB:
+			return "SUB";
+		case TEST:
+			return "TEST";
+		case XOR:
+			return "XOR";
+		case NOT:
+			return "NOT";
+		case SHL:
+			return "SHL";
+		case SHR:
+			return "SHR";
+		case PUT:
+			return "PUT";
+		case PUTC:
+			return "PUTC";
+		case JE:
+			return "JE";
+		case JG:
+			return "JG";
+		case JGE:
+			return "JGE";
+		case JL:
+			return "JL";
+		case JLE:
+			return "JLE";
+		case MUL:
+			return "MUL";
+		case RET:
+			return "RET";
+		case STOR:
+			return "STOR";
+		case LOAD:
+			return "LOAD";
+		default:
+			return "";
+	}
+}
+
+#endif //CVM_OPCODE_H
--- vm.h
+++ vm.h
@@ -13,113 +13,193 @@
 #include <stdio.h>
 
 // #include "../../uni/cs103/structs/stack.h"
-#include "instr.h"
+#include "opcode.h"
 
 #define STACK_SIZE 256
 #define DATA_SIZE 256
 
-#define TRACE 1
+
+#ifndef VM_TRACE
+#define VM_TRACE 0
+#else
+#undef VM_TRACE
+#define VM_TRACE 1
+#endif
+
+#ifndef TRACE_CHAR
+#define TRACE_CHAR 0
+#define STACK_FMT " %d"
+#else
+#undef TRACE_CHAR
+#define TRACE_CHAR 1
+#define STACK_FMT " %c"
+#endif
+
+
+#define pop2(x) x->reg1 = *x->sp, x->sp--; x->reg2 = *x->sp, x->sp--
+#define push2(x) *(++vm->sp) = vm->reg2; *(++vm->sp) = vm->reg1
+#define pop(x) x->reg1 = *x->sp, x->sp--
 
 typedef struct vm {
-	uint32_t* code;
-	uint32_t stack[STACK_SIZE];
-	uint32_t data[DATA_SIZE];
+	int8_t* code;
+	int32_t code_size;
+	int32_t* stack;
+	int32_t data[DATA_SIZE];
 	//registers
-	uint32_t* ip;
-	uint32_t* sp; //stack top
-	uint32_t* fp;
+	int8_t* ip;    // instruction pointer
+	int32_t* sp;    // stack top
+	int32_t* fp;    // frame pointer
+	int32_t reg1;
+	int32_t reg2;
 } VM;
 
-extern VM* vm_new(uint32_t* code, uint32_t size) {
+
+void print_stack(const uint32_t* base, const uint32_t* stack) {
+	while (stack > base) {
+		printf(STACK_FMT, *stack == 10 ? 32 : *stack);
+		stack--;
+	}
+}
+
+extern VM* vm_new(uint8_t* code, uint32_t size) {
 	VM* newvm = calloc(1, sizeof(VM));
-	newvm->code = calloc(size, sizeof(uint32_t));
-	memcpy(newvm->code, code, size * sizeof(uint32_t));
-	newvm->ip = newvm->code;
+	uint8_t* codeptr = (uint8_t*) calloc(size, sizeof(uint8_t));
+	newvm->code_size = size;
+	newvm->stack = calloc(STACK_SIZE, sizeof(uint32_t));
+	memcpy(codeptr, code, size * sizeof(uint8_t));
+	newvm->code = codeptr;
+	newvm->ip = codeptr;
 	newvm->sp = newvm->stack - 1;
 	newvm->fp = newvm->stack - 1;
 }
 
-extern void vm_run(VM* vm) {
-	uint32_t val1;
-	uint32_t val2;
-	while (1) {
-		instr_t instr = decode(&vm->ip);
-		if (TRACE)
-			printf("%0.4x %0.4x ", instr.opcode, instr.param);
-		switch (instr.opcode) {
-			case NOP:
-				break;
-			case HALT:
-				return;
-			case PUSH:
-				*(++vm->sp) = instr.param;
-				if (TRACE)
-					printf("PUSH %d", instr.param);
-				break;
-			case POP:
-				break;
-			case ADD:
-				val1 = *(vm->sp--);
-				val2 = *(vm->sp--);
-				*(++vm->sp) = val1 + val2;
-				break;
-			case SUB:
-				val1 = *(vm->sp--);
-				val2 = *(vm->sp--);
-				*(++vm->sp) = val1 - val2;
-				break;
-			case MUL:
-				val1 = *(vm->sp--);
-				val2 = *(vm->sp--);
-				*(++vm->sp) = val1 * val2;
-				break;
-			case DIV:
-				val1 = *(vm->sp--);
-				val2 = *(vm->sp--);
-				*(++vm->sp) = val1 / val2;
-				break;
-			case PRT:
-				val1 = *(vm->sp);
-				vm->sp--;
-				printf("%s%3d", TRACE ? "PUT " : "", val1);
-				break;
-			case PRTC:
-				val1 = *(vm->sp);
-				vm->sp--;
-				printf("%s%3c", TRACE ? "PUTC " : "", val1);
-				break;
-			case EQ:
-				val1 = *(vm->sp--);
-				val2 = *(vm->sp--);
-				*(++vm->sp) = val1 == val2;
-				break;
-			case NE:
-				val1 = *(vm->sp--);
-				val2 = *(vm->sp--);
-				*(++vm->sp) = val1 != val2;
-				break;
-			case LOAD:
-				val1 = instr.param; // data ddr
-				*(++vm->sp) = vm->data[val1];
-				if (TRACE)
-					printf("LOAD %3d %3d", val1);
-				break;
-			case STOR:
-				val1 = *(vm->sp); // value
-				vm->sp--;
-				val2 = instr.param; // data addr
-				vm->data[val2] = val1;
-				if (TRACE)
-					printf("STOR %3d %3d", val2, val1);
-				break;
-		}
-		if (TRACE)
-			printf("\n");
+static void print_code(VM* vm) {
+	for (int i = 0; i < vm->code_size; ++i) {
+		printf("0x%x ", vm->ip);
+		printf("0x%x %s\n", vm->code + i, mnemonic(*(vm->code + i)));
 	}
 }
 
-void execute(VM* vm, instr_t instr) {
-
+extern void vm_run(VM* vm) {
+	if (VM_TRACE) {
+		printf("%10s %6s %4s %5s\n", "Address", "Instr", "Opc", "Stack");
+	}
+	while (1) {
+		// print_code(vm);
+		opcode_t opcode = decode(vm->ip);
+		vm->ip++;
+		if (VM_TRACE) {
+			printf("0x%x 0x%0.2x %4s", vm->ip, vm->ip - vm->code, mnemonic(opcode));
+			print_stack(vm->fp, vm->sp);
+			printf("\n");
+		}
+		switch (opcode) {
+			case ADD:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 + vm->reg2;
+				break;
+			case AND:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 & vm->reg2;
+				break;
+			case CALL:
+				break;
+			case DEC:
+				pop(vm);
+				*(++vm->sp) = --vm->reg1;
+				break;
+			case DIV:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 / vm->reg2;
+				break;
+			case HLT:
+				return;
+			case INC:
+				pop(vm);
+				*(++vm->sp) = ++vm->reg1;
+				break;
+			case JMP:
+				vm->reg1 = next_32(vm->ip);
+				vm->ip += 4;
+				vm->ip += vm->reg1;
+				break;
+			case JMPB:
+				vm->reg1 = next_32(vm->ip);
+				vm->ip += 4;
+				vm->ip -= vm->reg1;
+				break;
+			case MOV:
+				// TODO: MOV
+				break;
+			case NOP:
+				break;
+			case OR:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 | vm->reg2;
+				break;
+			case POP:
+				vm->sp--;
+				break;
+			case PUSH:
+				*(++vm->sp) = next_32(vm->ip);
+				vm->ip += 4;
+				break;
+			case RET:
+				break;
+			case SUB:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 - vm->reg2;
+				break;
+			case TEST:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 == vm->reg2;
+				break;
+			case XOR:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 ^ vm->reg2;
+				break;
+			case NOT:
+				pop(vm);
+				*(++vm->sp) = ~vm->reg1;
+				break;
+			case SHL:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 << vm->reg2;
+				break;
+			case SHR:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 >> vm->reg2;
+				break;
+			case PUT:
+				// pop(vm);
+				printf("%d\n", *vm->sp);
+				break;
+			case PUTC:
+				// pop(vm);
+				printf("%c\n", *vm->sp);
+				break;
+			case MUL:
+			pop2(vm);
+				*(++vm->sp) = vm->reg1 + vm->reg2;
+				break;
+			case JE:
+				break;
+			case JG:
+				break;
+			case JGE:
+				break;
+			case JL:
+				break;
+			case JLE:
+				break;
+			case STOR:
+				break;
+			case LOAD:
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 #endif //CVM_VM_H
